0bbd25772cc12d9e997cfdfcbdf52869b2000b09
YashRaj <91825864+YashRajCodes@users.noreply.github.com>
2026-02-26 01:00:42 +0530

    feat(removeChatButtons): initial commit

---
+++ src/equicordplugins/removeChatButtons/index.tsx
@@ -0,0 +1,14 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { EquicordDevs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "RemoveChatButtons",
+    description: "Adds the ability to remove certain chat buttons.",
+    authors: [EquicordDevs.yash],
+});
